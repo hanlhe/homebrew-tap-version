98f6ea41940867bffafef45e283e596bc52bc588
Hanlin He <hanlhe@cisco.com>
2021-10-26 15:24:45 -0700

    Remove unneeded.

--- Formula/maven@3.6.3.rb
+++ Formula/maven@3.6.3.rb
@@ -12,8 +12,6 @@ class MavenAT363 < Formula
     regex(/href=.*?apache-maven[._-]v?(\d+(?:\.\d+)+)-bin\.t/i)
   end
 
-  bottle :unneeded
-
   depends_on "openjdk"
 
   conflicts_with "mvnvm", because: "also installs a 'mvn' executable"
